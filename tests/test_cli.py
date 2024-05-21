2307b9720f69ee42ae9407ecd1124d5d57827fa6
ivansantiagojr <ivansantiago.junior@gmail.com>
2024-05-21 02:03:50 -0300

    test: covering more files not found in build command

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 from typer.testing import CliRunner
 
 from resma_cli.main import app
@@ -20,3 +22,40 @@ def test_start_project_already_exists(temp_dir, monkeypatch):
 
     assert result.exit_code == 1
     assert 'File already exists' in result.output
+
+
+def test_build_resma_project_not_found(temp_dir, monkeypatch):
+    monkeypatch.chdir(temp_dir)
+
+    result = runner.invoke(app, ['build'])
+
+    assert result.exit_code == 1
+    assert 'Not a resma project' in result.output
+
+
+def test_build_not_a_index_html(temp_dir, monkeypatch):
+    monkeypatch.chdir(temp_dir)
+
+    runner.invoke(app, ['start', 'project'])
+
+    monkeypatch.chdir('project')
+    result = runner.invoke(app, ['build'])
+
+    assert result.exit_code == 1
+    assert 'Could not find an index.html template' in result.output
+
+
+def test_build_resma_table_not_found_in_config_toml(temp_dir, monkeypatch):
+    monkeypatch.chdir(temp_dir)
+    runner.invoke(app, ['start', 'project'])
+
+    monkeypatch.chdir('project')
+    (Path('.') / 'templates' / 'index.html').touch()
+
+    # erasing resma table from config.toml
+    with open('config.toml', 'w') as f:
+        f.write('')
+    result = runner.invoke(app, ['build'])
+
+    assert result.exit_code == 1
+    assert 'config.toml should have a resma table' in result.output
