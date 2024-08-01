10e1bf3d9d909e5bb2b4265a1cbc41bfd4d6d5a9
ivansantiagojr <ivansantiago.junior@gmail.com>
2024-08-01 06:34:16 -0300

    feat: add rel_path jinja global to get relative path to public static and styles folder to html files

---
+++ resma_cli/jinja_globals.py
@@ -0,0 +1,29 @@
+# get relative path function to be used from jinja template to img or href
+
+from typing import Literal
+
+from jinja2 import pass_context
+from jinja2.runtime import Context
+
+
+@pass_context
+def rel_path(
+    context: Context,
+    file_path: str,
+    resource_type: Literal['static', 'style'],
+) -> str:
+
+    levels_to_go_up = context['page']['depth']
+
+    relative_path_to_root = '../' * levels_to_go_up
+
+    match resource_type:
+        case 'style':
+            resource_path = f'{relative_path_to_root}public/styles/{file_path}'
+        case 'static':
+            resource_path = f'{relative_path_to_root}public/static/{file_path}'
+        case _:
+            raise ValueError('resource_type must be either static or style')
+
+    print(context['page']['depth'])
+    return resource_path
